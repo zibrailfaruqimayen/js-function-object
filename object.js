27c90c5b2bf55b6bddfeb3046ad46d605d3f06af
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-30 18:38:47 +0600

    function-object

---
+++ object.js
@@ -0,0 +1,24 @@
+var student = {
+    id: 115,
+    name: 'solaiman khan',
+    class: 9,
+    marks: 98
+}
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    Storage: '64gb',
+    camera: '7MP'
+}
+
+var myComputer = {
+    brand: 'lenovo',
+    price: 39000,
+    color: 'silver',
+    processor: 'i7'
+}
+
+console.log(myComputer.processor);
+myComputer.processor = 'i79';
+console.log(myComputer);
