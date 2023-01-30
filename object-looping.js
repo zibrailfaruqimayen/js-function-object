29ccb442986078e6c834fab6fe15e37375243fd8
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-30 19:45:48 +0600

    function-object-looping

---
+++ object-looping.js
@@ -0,0 +1,44 @@
+
+// array vs object
+var shoppingItems = ['books', 'sunglass', 'shoes', 'pen'];
+var friendsAge = [12, 45, 78, 12, 32, 14];
+var friendsAge = {
+    rahim: 12,
+    samad: 45,
+    karim: 78,
+    jamal: 12,
+    robi: 32,
+    kazi: 14
+}
+
+
+var shoppingCart = {
+    book: 3,
+    sunglass: 1,
+    keyboard: 5,
+    mouse: 1,
+    pen: 25,
+    shoes: 2,
+    
+}
+
+const keys = Object.keys(shoppingCart);
+console.log(keys);
+
+const values = Object.values(shoppingCart);
+console.log(values);
+
+
+// var keys = [ 'book', 'sunglass', 'keyboard', 'mouse', 'pen', 'shoes' ]
+
+for(var i = 0; i < keys.length; i++){
+    var propertyName = keys[i];
+    var propertyValues = shoppingCart[propertyName];
+    console.log(propertyName, propertyValues);
+}
+
+//for in loop
+for(var propertyName in shoppingCart){
+    const value = shoppingCart[propertyName];
+    console.log(propertyName, value);
+}
