b721027bb06636f4ed9f9542acedda9df5c79f86
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-30 17:31:35 +0600

    function return

---
+++ return.js
@@ -0,0 +1,22 @@
+function add(number1, number2){
+    console.log(number1, number2);
+    var sum = number1 + number2;
+    // console.log(sum);
+    return sum;
+}
+
+// add(45, 15);
+
+
+var total = add(80, 20);
+// console.log('total', total);
+
+function bringSingara(money){
+    var singaraPrice = 10;
+    var quantity = money / singaraPrice;
+    return quantity;
+}
+
+var myTaka = 150;
+var singaras = bringSingara(myTaka);
+console.log('eating singara: ', singaras);
