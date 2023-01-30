e5a2207a349c0a9ded5d87d881fdbce1d0493080
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-30 17:13:49 +0600

    function parameter

---
+++ parameter.js
@@ -0,0 +1,28 @@
+// function bringSingara(money){
+//     console.log('mama singara den');
+//     console.log(money);
+// }
+
+// bringSingara(250);
+
+function bringSingara(){
+    console.log('taka disen:', money);
+    console.log('ai nen singara');
+}
+
+var taka = 300;
+
+// bringSingara(taka);
+
+
+function add(num1, num2){
+    console.log('going to add', num1, num2);
+}
+
+// add(125, 96);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+} 
