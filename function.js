446457548cefb056fe6a79303332cdaa8ec72136
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-30 17:00:13 +0600

    function

---
+++ function.js
@@ -0,0 +1,14 @@
+// Function declaration
+function startFan(){
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+// Call the function
+startFan();
+console.log('Waking up in the morning')
+startFan();
+console.log('Eating lunch');
+console.log('Watching JS tutorial');
+startFan();
